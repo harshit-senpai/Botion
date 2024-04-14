9908d4ae532dc23ade3429f4d8f7cef88ff24bc1
harshit Mishra <miananya2@gmail.com>
2024-04-15 02:36:12 +0530

    second section

--- app/(Landing-page)/page.tsx
+++ app/(Landing-page)/page.tsx
@@ -1,11 +1,12 @@
 import Navbar from "@/components/navbar/Navbar";
 import HeroSection from "./hero-section";
-
+import StoriesSection from "./StoriesSection";
 export default function LandingPage() {
   return (
     <div>
       <Navbar />
       <HeroSection />
+      <StoriesSection />
     </div>
   );
 }
