e7c395704d36536a32838913b6613f7d25c57efb
harshit Mishra <miananya2@gmail.com>
2024-04-15 23:17:59 +0530

    final commit

--- app/(Landing-page)/page.tsx
+++ app/(Landing-page)/page.tsx
@@ -1,12 +1,26 @@
 import Navbar from "@/components/navbar/Navbar";
 import HeroSection from "./hero-section";
 import StoriesSection from "./StoriesSection";
+import Review from "./Review";
+import Blocks from "./Blocks";
+import Team from "./Team";
+import Slogan from "./Slogan";
+import UseCase from "./UseCase";
+import Banner from "./Banner";
+import Footer from "@/components/Footer";
 export default function LandingPage() {
   return (
     <div>
       <Navbar />
       <HeroSection />
       <StoriesSection />
+      <Review />
+      <Blocks />
+      <Team />
+      <Slogan />
+      <UseCase />
+      <Banner />
+      <Footer />
     </div>
   );
 }
